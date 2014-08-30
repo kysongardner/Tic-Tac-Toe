376a406e2e77e981c34a3ac06934ecbda5e4052b
kysongardner <kysongardner1@gmail.com>
2014-08-30 01:38:38 -0500

    Added Buttons and Values to them

--- TicTacToePrettyUI/MainWindow.xaml.cs
+++ TicTacToePrettyUI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,17 +13,56 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Tick_Tac_Toe;
 
 namespace TicTacToePrettyUI
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+        TTTEngine MyTicTacToeBoard = new TTTEngine(false);
         public MainWindow()
         {
+            
             InitializeComponent();
+            UpdateGameState();
+            this.DataContext = this;
+
+
         }
+
+        public int[][] GameState
+        {
+            get;
+            set;
+        }
+
+        private void NewGameClick(object sender, RoutedEventArgs e)
+        {
+            MyTicTacToeBoard = new TTTEngine(false);
+            UpdateGameState();
+        }
+        public void UpdateGameState()
+        {
+            this.GameState = MyTicTacToeBoard.CurrentBoardState();
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs("GameState"));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void TicTacClick(object sender, RoutedEventArgs e)
+        {
+            var Button = sender as Button;
+            var Position = Button.Tag.ToString().Split(',');
+            var MyPoint = new Tick_Tac_Toe.Point(Convert.ToInt32(Position[0]), Convert.ToInt32(Position[1]));
+            MyTicTacToeBoard.TryMove(MyPoint.row, MyPoint.column, 1);
+            UpdateGameState();
+        }
+       
     }
 }
